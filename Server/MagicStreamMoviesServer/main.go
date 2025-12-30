4a9f23138745ea3244f71e7c322a28e75d988fc4
PTnam018 <pnam83131@gmail.com>
2025-12-30 22:54:19 +0700

    register

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 	controllers "github.com/tuananhsilly/MagicStreamMovies/Server/MagicStreamMoviesServer/controllers"
-
 )
 
-func main(){
+func main() {
 	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context){
-		c.String(200, "Hello, MagicStreamMoviesServer!")  
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMoviesServer!")
 	})
 
 	router.GET("/movies", controllers.GetMovies())
 	router.GET("/movie/:imdb_id", controllers.GetMovie())
-	
+	router.POST("/register", controllers.RegisterUser())
+
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Error starting server: ", err)
 	}
